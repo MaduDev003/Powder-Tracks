91379ba2a930cffb5cf69bc066a0c7dc8efb0c92
MaduDev003 <mariabevenuto062@gmail.com>
2025-09-13 13:45:43 -0300

    test: images to service worker

--- service-worker.js
+++ service-worker.js
@@ -2,23 +2,21 @@ const CACHE_NAME = 'arcane-v1';
 const FILES_TO_CACHE = [
   './',
   './index.html',
-  './styles/app.css',
-  './styles/footer.css',
-  './styles/global.css',
-  './styles/header.css',
-  './styles/main.css',
+  './styles/global.css',   // só esse
   './scripts/svg-inject.min.js',
   './assets/icons/heart.svg',
   './assets/icons/minimize.svg',
   './assets/icons/pause.svg',
+  './assets/icons/chevron-left.svg',
+  './assets/icons/chevron-right.svg',
   './assets/icons/play.svg',
   './assets/icons/random.svg',
   './assets/icons/repeat.svg',
   './assets/icons/volume.svg',
   './assets/jinx.png',
   './assets/user.jpg',
-  './assets/fonts/GT-Eesti-Display-Medium.otf',
-  './assets/fonts/GT-Eesti-Display-Regular.otf'
+  './fonts/GT-Eesti-Display-Medium.otf',
+  './fonts/GT-Eesti-Display-Regular.otf'
 ];
 
 self.addEventListener('install', event => {
@@ -40,7 +38,6 @@ self.addEventListener('activate', event => {
   );
   self.clients.claim();
 });
-
 
 self.addEventListener('fetch', event => {
   event.respondWith(
