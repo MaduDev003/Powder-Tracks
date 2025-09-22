939913d7f350430a447b6d363d30f4d6e7753d98
MaduDev003 <mariabevenuto062@gmail.com>
2025-09-22 11:05:49 -0300

    feat: close svg

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,8 @@
 function listLinksToggle(){
     const links = document.querySelector('.links');
+    const closeLinks = document.querySelector('.close');
+    const listLinks = document.querySelector('.list');
+    closeLinks.classList.toggle('active');
+    listLinks.classList.toggle('active');
     links.classList.toggle('active');
 }
