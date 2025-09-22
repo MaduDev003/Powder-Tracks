988bd208c0c3dd39381b7f12ea820da16cbddb1c
MaduDev003 <mariabevenuto062@gmail.com>
2025-09-22 11:36:41 -0300

    feat:change pages function

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,21 @@
-function listLinksToggle(){
+function toggleListLinks() {
     const links = document.querySelector('.links');
     const closeLinks = document.querySelector('.close');
     const listLinks = document.querySelector('.list');
+
+    // Toggle menu mobile
     closeLinks.classList.toggle('active');
     listLinks.classList.toggle('active');
     links.classList.toggle('active');
+
+}
+
+
+function choosePage(event) {
+    let clickedLi = event.target.closest('li.link');
+    if (!clickedLi) return;
+
+    const allLinks = document.querySelectorAll('.links .link');
+    allLinks.forEach(link => link.classList.remove('active'));
+    clickedLi.classList.add('active');
 }
